cdff6ca9614acf0a3aa9f3b7b8cb0f08061a5306
ztalaei <139300102+ztalaei@users.noreply.github.com>
2024-02-02 11:25:29 +1000

    added frontend

---
+++ frontend/webpack.config.mjs
@@ -0,0 +1,106 @@
+// The path to the CesiumJS source code
+const cesiumSource = "node_modules/cesium/Source";
+const cesiumWorkers = "../Build/Cesium/Workers";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import HtmlWebpackHarddiskPlugin from "html-webpack-harddisk-plugin";
+import CopywebpackPlugin from "copy-webpack-plugin";
+import url from "url"; // Import the 'url' module
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
+// import { config as dotEnvConfig } from 'dotenv';
+// dotEnvConfig();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const config = {
+  context: __dirname,
+  mode: "development", // Use the environment variable we defined
+  entry: {
+    app: ["./public/index.mjs"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+
+    // Needed to compile multiline strings in Cesium
+    sourcePrefix: "",
+  },
+  // Configure caching for incremental builds
+  cache: {
+    type: "filesystem", // Use filesystem cache
+    buildDependencies: {
+      config: [import.meta.url], // Rebuild when webpack.config.js changes
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ["url-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      alwaysWriteToDisk: false, // Add this line
+      scripts: ["cesium/Cesium.js"],
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    // Copy Cesium Assets, Widgets, and Workers to a static directory
+    new CopywebpackPlugin({
+      patterns: [
+        { from: path.join(cesiumSource, cesiumWorkers), to: "Workers" },
+        { from: path.join(cesiumSource, "Assets"), to: "Assets" },
+        { from: path.join(cesiumSource, "Widgets"), to: "Widgets" },
+        { from: path.join(cesiumSource, "ThirdParty"), to: "ThirdParty" },
+        { from: "output/content", to: "output/content" },
+        {
+          from: "../mercator-transforms-master",
+          to: "mercator-transforms-master",
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify(""),
+    }),
+    new NodePolyfillPlugin(),
+  ],
+
+  // development server options
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    // hot: true,  // Enable hot module replacement
+  },
+  resolve: {
+    extensions: [".js", ".mjs", ".json"],
+    modules: ["node_modules"],
+    alias: {
+      // CesiumJS module name
+      cesium: path.resolve(__dirname, cesiumSource),
+    },
+    fallback: {
+      fs: false,
+      url: url.pathToFileURL("./node_modules/url/url.js").href,
+      zlib: false,
+    },
+  },
+  amd: {
+    // Enable webpack-friendly use of require in Cesium
+    toUrlUndefined: true,
+  },
+  watchOptions: {
+    ignored: /node_modules/, // Ignore everything in the node_modules directory
+  },
+};
+export default config;
